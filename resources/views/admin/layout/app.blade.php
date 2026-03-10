9914bfeb9efeb4a86831602d3d4d28f1ee0ef4e9
Piyush <mailto:deepak.tanwar@icon-solutions.us>
2026-03-10 21:28:17 +0530

    add

--- resources/views/admin/layout/app.blade.php
+++ resources/views/admin/layout/app.blade.php
@@ -1173,7 +1173,7 @@
             <!--begin::Copyright-->
             <strong>
                 Copyright &copy; 2014-2024&nbsp;
-                <a href="https://adminlte.io" class="text-decoration-none">AdminLTE.io</a>.
+                <a href="https://piyushkumawat.in" class="text-decoration-none">Piyush Kumawat </a>.
             </strong>
             All rights reserved.
             <!--end::Copyright-->
